e29e3f0b08772b56e0834803459fcecbcb0c21aa
devVenny <hyungsup42134@gmail.com>
2025-12-29 20:19:56 +0900

    Initial Commit

---
+++ DDCmusicload/DDCmusicloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDCmusicloadApp.swift
+//  DDCmusicload
+//
+//  Created by venny lee on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DDCmusicloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
